58e304e50227bea2596f333b1fdfc735241711db
Shivani Juwarkar <shivanijuwarkar@gmail.com>
2021-01-20 16:18:07 +0530

    First Commit

---
+++ src/main/java/com/main/page/PageClass.java
@@ -0,0 +1,34 @@
+package com.main.page;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class PageClass {
+
+    private final WebDriver driver;
+    private final WebDriverWait wait;
+
+    public PageClass(final WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+        this.wait = new WebDriverWait(driver, 30);
+    }
+
+    public void goTo() {
+        this.driver.get("https://www.google.com");
+        System.out.println("Browser launched and navigated to Google");
+    }
+
+    public boolean checkTitle(String expectedTitle) {
+        System.out.println(driver.getTitle().contains(expectedTitle));
+        boolean x = driver.getTitle().contains(expectedTitle);
+        return x;
+    }
+
+}
---
+++ src/test/java/com/main/test/TestClass.java
@@ -0,0 +1,40 @@
+package com.main.test;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import com.main.page.PageClass;
+public class TestClass {
+
+	private WebDriver driver;
+    private PageClass google;
+
+    @BeforeTest
+    public void setUp() throws MalformedURLException { 
+		driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.chrome());
+        driver.manage().window().maximize();
+        google = new PageClass(driver);
+    }
+
+    @Test
+    public void googleTest() {
+        google.goTo();
+        Assert.assertTrue(google.checkTitle("u"));
+    }
+    
+    @AfterTest
+    public void tearDown() throws InterruptedException {
+        driver.quit();
+    }   
+
+}
