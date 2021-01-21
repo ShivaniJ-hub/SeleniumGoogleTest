8abd5255b2b8188c4b3fd65cb258b6fe9870b0ac
ShivaniJ-hub <71375978+ShivaniJ-hub@users.noreply.github.com>
2021-01-21 23:34:59 +0530

    Update TestClass.java

--- src/test/java/com/main/test/TestClass.java
+++ src/test/java/com/main/test/TestClass.java
@@ -38,7 +38,8 @@ public class TestClass {
     @Test
     public void googleTest() {
         google.goTo();
-        Assert.assertTrue(google.checkTitle("Store"));
+        //Assert.assertTrue(google.checkTitle("Store"));
+	Assert.assertTrue(google.checkTitle("Player"));
     }
     
     @AfterTest
